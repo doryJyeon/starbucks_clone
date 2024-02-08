8e095b2b9fba853f85008ed8c0cc95df173b6c64
JYeon <doryJYeon@gmail.com>
2024-02-08 23:44:53 +0900

    [+] section1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+// search
 const searchBox = document.querySelector("#searchBox");
 const searchInput = searchBox.querySelector("input");
 
-searchBox.addEventListener("click", function () {
+searchBox.addEventListener("click", () => {
   searchInput.focus();
   searchBox.classList.add("focused");
   searchInput.setAttribute("placeholder", "search");
@@ -10,4 +11,30 @@ searchBox.addEventListener("click", function () {
 searchInput.addEventListener("blur", () => {
   searchBox.classList.remove("focused");
   searchInput.setAttribute("placeholder", "");
+});
+
+//header hide
+const badgeBox = document.querySelector("header .badges");
+
+window.addEventListener("scroll",_.throttle(() => {
+  window.scrollY > 500
+    ? gsap.to(badgeBox, 0.4, { opacity: 0, display: "none" })
+    : gsap.to(badgeBox, 0.4, { opacity: 1, display: "block" });
+}, 300));
+
+//fade-in
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+
+fadeEls.forEach((fadeEl, index) => {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
+
+// Swiper
+new Swiper(".notice_line .swiper", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true
 });
